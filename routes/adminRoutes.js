d49fe21b1cfade2175ace45e84e9811b943fecf4
W3-92653-vijay <vijayshinde152002@gmail.com>
2026-06-06 11:52:30 +0530

    Admin route

---
+++ routes/adminRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const pool = require('./db');
+const authMiddleware = require('./authMiddleware');
+const router = express.Router();
