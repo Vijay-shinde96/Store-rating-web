34f5b18dda4f69fcd61e8597aec7300e1ffde83f
W3-92653-vijay <vijayshinde152002@gmail.com>
2026-06-06 14:33:55 +0530

    owner Update password

--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -4,4 +4,13 @@ const pool = require('../db');
 const authMiddleware = require('../authMiddleware');
 const router = express.Router();
 
+
+// Update password
+router.put('/update-password', authMiddleware('owner'), async (req, res) => {
+  const { newPassword } = req.body;
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  await pool.query('UPDATE users SET password = ? WHERE id = ?', [hashedPassword, req.user.id]);
+  res.json({ message: 'Password updated successfully' });
+});
+
 module.exports = router;
